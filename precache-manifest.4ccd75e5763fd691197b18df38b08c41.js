9912951dbd337ae5d6377bdf9c1858c011075334
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-08-15 14:28:29 +0530

    Website with ReactJs

---
+++ precache-manifest.4ccd75e5763fd691197b18df38b08c41.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28c531a03566c927b13db334b1155cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03d758281dce7f8390ba",
+    "url": "/static/css/main.7d96df50.chunk.css"
+  },
+  {
+    "revision": "ca291ac208866a04b384",
+    "url": "/static/js/2.94534ca4.chunk.js"
+  },
+  {
+    "revision": "03d758281dce7f8390ba",
+    "url": "/static/js/main.f5407625.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "081fff63df8b087a25261c8019c403c8",
+    "url": "/static/media/0.081fff63.jpg"
+  },
+  {
+    "revision": "0904b5505a2c9f39585d67b603844739",
+    "url": "/static/media/header.0904b550.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3296fa6e1dc60baf2769df6f8b4e9100",
+    "url": "/static/media/sololearn.3296fa6e.png"
+  }
+]);
