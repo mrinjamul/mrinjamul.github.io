a631121d51d7db4c7d67dc01fe45cdca4fb4e098
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-08-16 20:37:56 +0530

    Add service worker

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ad89eb6fdbab7774ca1264a8da6abbba.js"
+  "/precache-manifest.4eb112fb4c5c18c4ca15034bcbe4f136.js"
 );
 
 self.addEventListener('message', (event) => {
